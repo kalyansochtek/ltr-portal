da8b4a0b14e6c0f2107f11ba379a4131f8273d7d
kalyan <kalyansochtek@gmail.com>
2026-05-22 11:53:15 +0530

    final sam api fix

--- api/sam.js
+++ api/sam.js
@@ -2,17 +2,12 @@ export default async function handler(req, res) {
   try {
     const API_KEY = process.env.VITE_SAM_API_KEY;
 
-    console.log("API KEY:", API_KEY);
+    const url =
+      `https://api.sam.gov/prod/opportunities/v2/search?api_key=${API_KEY}&limit=5&offset=0`;
 
-    const response = await fetch(
-      `https://api.sam.gov/opportunities/v2/search?limit=1&api_key=${API_KEY}`
-    );
-
-    console.log("STATUS:", response.status);
+    const response = await fetch(url);
 
     const text = await response.text();
-
-    console.log("RAW RESPONSE:", text);
 
     res.status(200).json({
       status: response.status,
