de957cff23e0305a95399760859d3aec391c23a6
kalyan <kalyansochtek@gmail.com>
2026-05-22 12:25:51 +0530

    fix sam api params

--- api/sam.js
+++ api/sam.js
@@ -1,11 +1,13 @@
 export default async function handler(req, res) {
   try {
-    const API_KEY = process.env.SAM_API_KEY;
+    const apiKey = process.env.SAM_API_KEY;
 
-    const response = await fetch(
-      `https://api.sam.gov/prod/opportunities/v2/search?api_key=${API_KEY}&limit=1`
-    );
+    const postedFrom = "01/01/2025";
+    const postedTo = "12/31/2025";
 
+    const url = `https://api.sam.gov/prod/opportunities/v2/search?api_key=${apiKey}&limit=5&postedFrom=${postedFrom}&postedTo=${postedTo}`;
+
+    const response = await fetch(url);
     const data = await response.json();
 
     res.status(200).json(data);
