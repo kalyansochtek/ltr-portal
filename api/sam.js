867de8a09fd0d17f3504f6ef5915489491249ba5
kalyan <kalyansochtek@gmail.com>
2026-05-22 11:58:59 +0530

    fixed

--- api/sam.js
+++ api/sam.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
     const API_KEY = process.env.VITE_SAM_API_KEY;
 
     const response = await fetch(
-      `https://api.sam.gov/prod/opportunities/v2/search?api_key=${API_KEY}&limit=1`
+      `https://api.sam.gov/prod/opportunities/v2/search?api_key=YOUR_KEY&limit=1`
     );
 
     const data = await response.json();
