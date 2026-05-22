3bb4ba319628e3158fd577470ddeb8321d3cb6b8
kalyan <kalyansochtek@gmail.com>
2026-05-22 11:43:28 +0530

    fixed sam api

--- api/sam.js
+++ api/sam.js
@@ -1,23 +1,28 @@
 export default async function handler(req, res) {
-
-  const API_KEY = process.env.VITE_SAM_API_KEY;
-
   try {
+    const API_KEY = process.env.VITE_SAM_API_KEY;
 
     const response = await fetch(
       `https://api.sam.gov/prod/opportunities/v2/search?api_key=${API_KEY}&limit=5`
     );
 
-    const data = await response.json();
+    const text = await response.text();
 
-    res.status(200).json(data);
+    console.log(text);
 
-  } catch (error) {
-
+    try {
+      const data = JSON.parse(text);
+      res.status(200).json(data);
+    } catch {
+      res.status(500).json({
+        error: "SAM API failed",
+        details: text,
+      });
+    }
+  } catch (err) {
     res.status(500).json({
-      error: "SAM API failed",
-      details: error.message,
+      error: "Server Error",
+      details: err.message,
     });
-
   }
 }
