d528c440c6bf14f5fee2c0e8428a580a1d7af0ad
kalyan <kalyansochtek@gmail.com>
2026-05-22 15:48:07 +0530

    update

--- src/api.js
+++ src/api.js
@@ -7,31 +7,23 @@ export async function fetchSAMData() {
     console.log("SAM DATA:", data);
 
     return data.opportunitiesData.map((x, i) => ({
-      id: i + 1,
-      title: x.title || "No Title",
-      agency:
-        x.fullParentPathName ||
-        x.organizationType ||
-        "N/A",
-      sector: x.type || "Federal",
-      setAside:
-        x.typeOfSetAsideDescription || "None",
-      vehicle: x.naicsCode || "N/A",
-      stage: "CAPTURE",
-      decision: "PRIME",
-      pwin:
-        Math.floor(Math.random() * 40) + 40,
-      value:
-        x.award?.amount
-          ? `$${(x.award.amount / 1000000).toFixed(1)}M`
-          : "$5M",
-      margin:
-        Math.floor(Math.random() * 10) + 10,
-      due:
-        x.responseDeadLine
-          ? x.responseDeadLine.split("T")[0]
-          : "N/A",
-    }));
+  id: i + 1,
+  title: x.title || "No Title",
+  agency: x.fullParentPathName || x.organizationType || "N/A",
+  sector: "Federal",
+  setAside: x.typeOfSetAsideDescription || "None",
+  vehicle: x.naicsCode || "N/A",
+  stage: "Identify",
+  decision: "PRIME",
+  pwin: Math.floor(Math.random() * 40) + 40,
+  value: x.award?.amount
+    ? parseFloat((x.award.amount / 1000000).toFixed(1))
+    : 0,
+  margin: Math.floor(Math.random() * 10) + 10,
+  due: x.responseDeadLine
+    ? x.responseDeadLine.split("T")[0]
+    : "N/A",
+}));
 
   } catch (error) {
     console.error("SAM API Error:", error);
